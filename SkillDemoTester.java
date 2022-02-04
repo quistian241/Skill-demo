8d326c74634d9da65f81f198dc3eab0d4303fa68
quistian241 <c7gonzalez@ucsd.edu>
2022-02-03 23:58:50 -0800

    Update SkillDemoTester.java

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTester {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.subtract(4, 1));
+        assertEquals(2, SkillDemo.subtract(4, 2));
     }
 }
